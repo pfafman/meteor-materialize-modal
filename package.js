f4f1468c6ad147793327d07839bd3e88c9ad1fd3
Timothy Pfafman <tim@pfafman.com>
2015-03-17 11:50:27 -0700

    do fixed footer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-    name: "pfafman:materialize-modal",
-    summary: "Display a modal via Materialize written in coffeescript",
+  name: "pfafman:materialize-modal",
+  summary: "Display a modal via Materialize written in coffeescript",
   version: "0.1.3",
   git: "https://github.com/pfafman/meteor-materialize-modal.git"
 });
@@ -8,31 +8,31 @@ Package.describe({
 Package.on_use(function(api, where) {
   api.versionsFrom("METEOR@1.0");
 
-    api.use([
-        'underscore',
-        'templating',
-        'ui',
-        'less',
-        'jquery',
-        'coffeescript',
-        'reactive-var',
-    ], 'client');
+  api.use([
+    'underscore',
+    'templating',
+    'ui',
+    'less',
+    'jquery',
+    'coffeescript',
+    'reactive-var',
+  ], 'client');
 
-    api.add_files([
-        'lib/modal.less',
-        'lib/modal.html',
-        'lib/modal.coffee'
-    ], 'client');
+  api.add_files([
+    'lib/modal.less',
+    'lib/modal.html',
+    'lib/modal.coffee'
+  ], 'client');
 
-    if (api.export) {
-        api.export('MaterializeModal')
+  if (api.export) {
+    api.export('MaterializeModal')
     }
 
 });
 
 
 Package.on_test(function(api) {
-    api.use("pfafman:materialize-moda", 'client');
-    api.use(['tinytest', 'test-helpers', 'coffeescript'], 'client');
-    api.add_files('modal_tests.coffee', 'client');
+  api.use("pfafman:materialize-moda", 'client');
+  api.use(['tinytest', 'test-helpers', 'coffeescript'], 'client');
+  api.add_files('modal_tests.coffee', 'client');
 });
